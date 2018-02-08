717e656cb550e6b6b228bd11ed34411cae90a2b9
Alan Ramos <alan.ramos@jarsa.com.mx>
2018-02-08 10:18:31 -0600

    [ADD][openacademy] first commit

---
+++ openacademy/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright YEAR(S), AUTHOR(S)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Openacademy',
+    'summary': 'Summary of the module',
+    'version': '10.0.1.0.0',
+    'category': 'Academy',
+    'website': 'https://www.jarsa.com.mx/',
+    'author': 'Jarsa Sistemas',
+    'license': 'AGPL-3',
+    'installable': True,
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/openacademy_course.xml',
+    ],
+}
